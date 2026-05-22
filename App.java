25c58a2042905d36ab8e09048fbf14b1e253c5e5
Vamsi <vamsib0609@gmail.com>
2026-05-22 14:52:18 +0530

    Converted normal switch to rule switch

--- App.java
+++ App.java
@@ -11,13 +11,13 @@ public class App{
             System.out.println("1.Display All Students\n2.Add new Student\n3.Search Student by ID\n4.Update Student\n5.Exit");
             System.out.print("Please enter the choice: ");
             int choice = sc.nextInt();
-            switch(choice){
-                case 1:
+            switch(choice) {
+                case 1 -> {
                     for(Student std: studs.AllStudentInfo()){
                         System.out.println("ID: "+ std.getId() + " Name: " + std.getName() + " Branch: " + std.getBranch());
                     }
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.print("Enter ID: ");
                     int id = sc.nextInt();sc.nextLine();
                     System.out.print("Enter Name: ");
@@ -26,21 +26,20 @@ public class App{
                     String branch = sc.nextLine();
                     studs.addStudent(new Student(id, name, branch));
                     System.out.println("Added the Student " + name + " with ID: " + id + " & Branch: "+ branch + " into database successfully.");
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     System.out.print("Enter the Student ID to search the database: ");
                     int student_id = sc.nextInt();
                     Student searched_student = studs.searchStudent(student_id);
                     if(searched_student == null){
                         System.out.println("Student is not there in our database.");
                         System.out.println("Student didn't Found.");
-                    }else{
+                    } else {
                         System.out.println("Student Found Successfully!");
                         System.out.println("ID: "+ searched_student.getId() + " Name: " + searched_student.getName() + " Branch: " + searched_student.getBranch());
                     }
-                    break;
-                case 4:
-
+                }
+                case 4 -> {
                     System.out.print("Enter Student ID to update: ");
                     int update_id = sc.nextInt();
                     sc.nextLine();
@@ -59,16 +58,13 @@ public class App{
                     else{
                         System.out.println("Student Not Found!");
                     }
-
-                    break;
-
-                case 5:
+                }
+                case 5 -> {
                     System.out.println("Exiting from the Student Management System Application");
                     return;
-                default:
-                    System.out.println("Invalid Choice");
-                    break;
-            }   
+                }
+                default -> System.out.println("Invalid Choice");
+            }
         }
     }
 }
